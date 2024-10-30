5680edb472dc9b63bc07bdd7b44a9a74c857131a
Mostafa Ghorab <vetghorab@gmail.com>
2024-10-30 21:01:11 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <h1 className="text-3xl text-white underline">Hello, Three.js</h1>
+    </main>
+  )
+}
+
+export default App
